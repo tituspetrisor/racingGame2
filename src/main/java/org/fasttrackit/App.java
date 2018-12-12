f6dca7553e0658a3279f687f618c6179330b649d
tituspetrisor <petrisortit@gmail.com>
2018-12-12 21:27:22 +0200

    Variables, data types

---
+++ src/main/java/org/fasttrackit/App.java
@@ -0,0 +1,36 @@
+package org.fasttrackit;
+
+/**
+ * Hello world!
+ *
+ */
+public class App {
+    public static void main(String[] args) {
+        //declaring variable and instantiating a car object
+        // this is also called initializing a variable
+        Car firstCompetitor= new Car();
+        firstCompetitor.name = "Dacia";
+        firstCompetitor.color = "black";
+        firstCompetitor.doorCount = 5;
+        firstCompetitor.mileage = 9.5;
+        firstCompetitor.running = false;
+//local varibles are variables declared inside a methode
+//        they don'r receive default value
+        Engine firstEngine = new Engine();
+        firstEngine.manufacturer = "Renault";
+//sout print the message to the console
+        System.out.println(firstEngine.manufacturer);
+
+        firstCompetitor.engine = firstEngine;
+        firstCompetitor.engine.manufacturer = "BMW";
+        firstCompetitor = null; //golire variabile
+
+        System.out.println(firstEngine.manufacturer);
+//        System.out.println(firstEngine.manufacturer);
+//        Engine testEngine;
+
+
+    }
+    }
+
+
---
+++ src/main/java/org/fasttrackit/Car.java
@@ -0,0 +1,13 @@
+package org.fasttrackit;
+
+public class Car {
+//    instance variables(declared as properties of a class
+//    they receive default values (null for objects, 0 for numbers, false for booleans)
+    String name;
+    String color;
+    int doorCount;
+    double mileage;
+    boolean running;
+    long totalKm;
+    Engine engine;
+}
